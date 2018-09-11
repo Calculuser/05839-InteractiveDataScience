a73d493a3c9045e3e18ba94e99a3c3bba9caef7b
Calculuser <alvin.zuo0923@gmail.com>
2018-09-11 15:27:19 -0400

    Modify API_KEY and TABLE_ID.

--- Assignments/Byte 2 - Explore/main.py
+++ Assignments/Byte 2 - Explore/main.py
@@ -24,14 +24,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # This API key is provided by google as described in the tutorial
-API_KEY = 'XXxxXxXXXXxxNXXxXXXxxxNNXXxxxxxxxXXXxXX'
+API_KEY = 'AIzaSyCrH5OIOLJGSnBhHtrjcTHqbrnAjor2bQk'
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 # This is the table id for the fusion table
-TABLE_ID = 'NxxxNXxXxxNxXXXXNXxXXXxXxxxNxXxNxXxxXxxX'
+TABLE_ID = '1yxJ8PZdzYcAn6auexe-lm1iWgOp1xcKmFmvKO_XK'
 
 # This is the default columns for the query
 query_cols = []
